0a838c429ebb6a0c8542d2eb10e352818333c456
mayu.yamamoto <y.mayutamn@gmail.com>
2021-11-10 04:14:40 +0000

    kadai-ruby-2

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,31 @@
+def fizzbuzz(num)
+    num = 1
+    while (num <= 100) do
+        if num % 3
+            puts Fizz
+    　　elsif num % 5
+    　　 　puts Buzz
+    　　elsif num % 3 && % 5
+　　       puts FizzBuzz
+    　　else
+    　　    puts result = num
+    　　end
+    　　
+    　　num_max = 100
+　　end
+
+
+def fizzbuzzz(num)
+    num = 1
+    while (num <= 100) do
+        if num 3
+            puts Fizz
+        elsif num 5
+            puts Buzz
+        elsif num 10 && 11
+            puts FizzBuzz
+        else
+            puts result = num
+        end
+    end
+end
