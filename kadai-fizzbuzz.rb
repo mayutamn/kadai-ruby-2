dd7ab5aea4ac2570f8023f739c55f929ebae9f1c
mayu.yamamoto <y.mayutamn@gmail.com>
2021-11-14 14:13:03 +0000

    kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,31 +1,46 @@
 def fizzbuzz(num)
-    num = 1
-    while (num <= 100) do
-        if num % 3
-            puts Fizz
-    　　elsif num % 5
-    　　 　puts Buzz
-    　　elsif num % 3 && % 5
-　　       puts FizzBuzz
-    　　else
-    　　    puts result = num
-    　　end
-    　　
-    　　num_max = 100
-　　end
-
-
-def fizzbuzzz(num)
-    num = 1
-    while (num <= 100) do
-        if num 3
-            puts Fizz
-        elsif num 5
-            puts Buzz
-        elsif num 10 && 11
-            puts FizzBuzz
-        else
-            puts result = num
-        end
+    if num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+    elsif num % 5 == 0
+    return  "Buzz"
+    elsif num % 3 ==0 
+    return "Fizz"
+    else
+    return num
     end
 end
+
+# def f(num)
+#     fizzbuzz(num)
+#     (num). each do |num|
+#     puts num_max = 100
+
+
+
+
+num_max = 100
+
+# (1..num_max).each do |number|
+#   puts number
+# end
+
+# puts 1
+# puts 2
+# puts 3
+# ...
+# puts 100
+
+(1..num_max).each do |number|
+  puts fizzbuzz(number)
+end
+
+# puts fizzbuzz(3)
+# puts fizzbuzz(5)
+# puts fizzbuzz(15)
+# puts fizzbuzz(1)
+
+# puts fizzbuzz(1)
+# puts fizzbuzz(2)
+# puts fizzbuzz(3)
+# ...
+# puts fizzbuzz(100)
